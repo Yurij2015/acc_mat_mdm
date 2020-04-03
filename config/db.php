ea30c2dac7647e6e742c3398ebfe77562b5d774b
yurij <nabor_2008@i.ua>
2020-04-03 06:25:03 -0400

    init commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mysql_acc;dbname=accounting',
+    'username' => 'user',
+    'password' => 'password2019',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
